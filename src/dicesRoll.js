c4a08def19fc682686c0ba97bec19aa1342a6172
Clément <clement.caloin@gmail.com>
2020-05-23 17:44:52 +0200

    Security change
    Changed random value generation to a secure one

--- src/dicesRoll.js
+++ src/dicesRoll.js
@@ -1,7 +1,9 @@
 export function dicesRoll() {
     let dices = 0;
+    let dice = new Uint8Array(1);
     for (let i = 0; i < 5; i++) {
-        dices += (~~(Math.random() * 6) + 1) * Math.pow(10, i)
+        window.crypto.getRandomValues(dice);
+        dices += (~~((dice[0] / 255) * 6) + 1) * Math.pow(10, i)
     }
     return dices
 }
